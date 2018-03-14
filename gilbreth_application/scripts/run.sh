58bb849253ad0f8ae81d61835e5ebc3e2dc44025
alex07zzz <alex_zyz@hotmail.com>
2018-03-14 19:43:17 -0400

    deleted old gripper
    modified gripper
    
    modified params
    
    added robot arm controller
    
    modified controller params
    
    a slower velocity param
    
    resolved problems
    
    new execution code
    
    added robot controller
    
    deleted traj node
    
    added test and exec node

--- gilbreth_application/scripts/run.sh
+++ gilbreth_application/scripts/run.sh
@@ -2,7 +2,7 @@
 echo "Start Demo"
 echo "Moving Conveyor Belt"
 rosservice call /gilbreth/conveyor/control "state:
-  power: 20.0" 
+  power: 10.0" 
 echo "Start Spawning Objects"
 rosservice call /start_spawn
 echo "Start kinect_publisher"
